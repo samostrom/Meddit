f07715e87453c66666a4de3eb2f1c1a5705ddd68
Sam Ostrom <sostrom@gmail.com>
2020-08-29 14:41:00 -0700

    setting up methodOveride

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const methodOverride = require('method-override');
+const env = require('dotenv').config()
+require('./config/database');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var frontPageRouter = require('./routes/frontPage');
 
 var app = express();
 
@@ -18,9 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/frontPage', frontPageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
